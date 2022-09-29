0a1963d0457cea1ce7df0fdff9f245564fae6f1b
Junebhone <bhonekyawsihein@gmail.com>
2022-09-29 14:21:37 +0630

    ios icon done

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,10 +1,17 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const SettingScreen = () => {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>SettingScreen</Text>
+      <Text>
+        <AntDesign
+          name="customerservice"
+          style={{color: 'red', fontSize: 50}}
+        />
+        <AntDesign name="windows" style={{color: 'red', fontSize: 50}} />
+      </Text>
     </View>
   );
 };
